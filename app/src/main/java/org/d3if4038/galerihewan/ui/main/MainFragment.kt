0d9f12d60eff5f0ec6e3e8bbde241ec6c1522dc5
Yolando Asri Erbenca Gegeh <yolandomanullang@student.telkomuniversity.ac.id>
2021-05-05 01:33:13 +0700

    3.6.Menambahkan loading indicator

--- app/src/main/java/org/d3if4038/galerihewan/ui/main/MainFragment.kt
+++ app/src/main/java/org/d3if4038/galerihewan/ui/main/MainFragment.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 
 import org.d3if4038.galerihewan.databinding.FragmentMainBinding
+import org.d3if4038.galerihewan.network.ApiStatus
 
 class MainFragment : Fragment() {
     private val viewModel: MainViewModel by lazy {
@@ -37,5 +38,23 @@ class MainFragment : Fragment() {
         viewModel.getData().observe(viewLifecycleOwner, {
             myAdapter.updateData(it)
         })
+        viewModel.getStatus().observe(viewLifecycleOwner, {
+            updateProgress(it)
+        })
     }
+    private fun updateProgress(status: ApiStatus) {
+        when (status) {
+            ApiStatus.LOADING -> {
+                binding.progressBar.visibility = View.VISIBLE
+            }
+            ApiStatus.SUCCESS -> {
+                binding.progressBar.visibility = View.GONE
+            }
+            ApiStatus.FAILED -> {
+                binding.progressBar.visibility = View.GONE
+                binding.networkError.visibility = View.VISIBLE
+            }
+        }
+    }
+
 }
